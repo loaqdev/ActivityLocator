35c2b8f0014a63437b39adb07cc5e6e28df5ae14
loaqdev <ponchik22893@gmail.com>
2025-07-27 23:09:15 +0200

    feat: Integrate React Query for activity management and refactor components

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -11,8 +11,6 @@ type Props = {
     openForm: (id: string) => void;
     closeForm: () => void;
     editMode: boolean;
-    submitForm: (activity: Activity) => void;
-    deleteActivity: (id: string) => void;
 }
 
 export default function ActivityDashboard({ activities, cancelSelectActivity,
@@ -20,31 +18,28 @@ export default function ActivityDashboard({ activities, cancelSelectActivity,
     selectedActivity,
     openForm,
     closeForm,
-    editMode,
-    submitForm,
-    deleteActivity: develeActivity
+    editMode,   
 }: Props) {
     return (
         <Grid container spacing={3}>
             <Grid size={7}>
                 <ActivityList
                     activities={activities}
-                    selectActivity={selectActivity}
-                    deleteActivity={develeActivity}
+                    selectActivity={selectActivity}                 
                 />
             </Grid>
             <Grid size={5}>
                 {selectedActivity && !editMode &&
                     <ActivityDetail
-                        activity={selectedActivity}
+                        selectedActivity={selectedActivity}
                         cancelSelectActivity={cancelSelectActivity}
                         openForm={openForm}
                     />
                 }
                 {editMode &&
                     <ActivityForm
-                        closeForm={closeForm} activity={selectedActivity}
-                        submitForm={submitForm}
+                        closeForm={closeForm} 
+                        activity={selectedActivity}                       
                     />}
             </Grid>
         </Grid>

67bc2dfc0b7af3c81b5272577c7dbb498836fd2d
loaqdev <ponchik22893@gmail.com>
2025-08-03 23:01:28 +0200

    feat: Add activity detail and chat components, implement filters, and enhance home page layout

--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -1,9 +1,47 @@
-import { Container, Typography } from "@mui/material";
+import { Group } from "@mui/icons-material";
+import { Box, Button, Paper, Typography } from "@mui/material";
+import { Link } from "react-router";
 
 export default function HomePage() {
     return (
-        <Container sx={{ mt: 3 }}>
-            <Typography variant="h3">Home page</Typography>
-        </Container>
+        <Paper
+            sx={{
+                color: "white",
+                display: "flex",
+                flexDirection: "column",
+                gap: 6,
+                alignItems: "center",
+                alignContent: "center",
+                justifyContent: "center",
+                height: "100vh",
+                backgroundImage: 'linear-gradient(135deg, #4b6cb7 0%, #182848 70%)'
+            }}
+        >
+            <Box sx={{
+                display: "flex",
+                alignItems: "center",
+                alignContent: "center",
+                color: "white",
+                gap: 3
+            }}
+            >
+                <Group sx={{ height: 110, width: 110 }} />
+                <Typography variant="h1">
+                    Activity Locator
+                </Typography>
+            </Box>
+            <Typography variant="h2">
+                Welcome to activity locator
+            </Typography>
+            <Button
+                component={Link}
+                to="/activities"
+                size="large"
+                variant="contained"
+                sx={{ height: 80, borderRadius: 4, fontSize: "1.5rem" }}
+            >
+                Take me to the activities
+            </Button>
+        </Paper>
     )
 }
